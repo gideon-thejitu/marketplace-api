b1de1fb971190e50075a670b8586db038908f9fe
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2024-02-27 08:48:16 +0300

    feat: update containers

--- Marketplace.Database/hangfire.sql
+++ Marketplace.Database/hangfire.sql
@@ -1,10 +1,10 @@
-IF DB_ID('Hangfire') IS NULL
+-- IF DB_ID('Hangfire') IS NULL
 BEGIN
     CREATE DATABASE [Hangfire]
 END
 GO
 
-IF DB_ID('MarketplaceDev') IS NULL
+-- IF DB_ID('MarketplaceDev') IS NULL
 BEGIN
     CREATE DATABASE [MarketplaceDev]
 END
